1e3a61503ba13cd30caaea9d5b659af19e718ee9
nshimiyimana <njeanluc828@gmail.com>
2020-12-14 12:16:25 +0200

    usephrase+resultinsteadphrase+n1+n2iftheseallbestr

--- app.ts
+++ app.ts
@@ -2,14 +2,15 @@ console.log("write some code here...");
 console.log("this is my first typeScript code");
 let message: string = 'Hello World';
 console.log(message);
-function add(n1:number, n2:number,showResult:boolean,phrase:string) {
+function add(n1, n2:number,showResult:boolean,phrase:string) {
 
    /*  console.log(typeof n1);
     if(typeof n1!=='number' || typeof n2!='number'){
         throw new Error('incorrect input');
     } */
+    const result=n1+n2;
     if(showResult){
-        console.log(phrase+ n1+n2);
+        console.log(phrase+result);
     }
     else{
  console.log(n1 + n2);
@@ -17,7 +18,9 @@ function add(n1:number, n2:number,showResult:boolean,phrase:string) {
     }
     
 }
-var number1=5;
+
+var number1:number;
+number1=5;
 var number2=2.7;
 
 const printResult=true;

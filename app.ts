ebfc3d6ccfedde5f248e08e97eee0402f00205b1
nshimiyimana <njeanluc828@gmail.com>
2020-12-14 16:51:41 +0200

    to get hobby property with array type

--- app.ts
+++ app.ts
@@ -1,10 +1,20 @@
 const Person: {
     name:string;
-    age:number
+    age:number,
+    hobbies:string[]
 }={
     name:'maximillian',
-    age:30
+    age:30,
+    hobbies:['sports','cooking']
 }
-console.log(Person);
 //if we get unexisted property ide tells us and ts tell it in terminal if you compile ts into js
+
+
+let FavoriteActivities:string[];  //or any[]
+FavoriteActivities=['sports','cooking'];
+//to get person name
 console.log(Person.name);
+//to get hobbies of person
+for(const hobbies of Person.hobbies){
+    console.log(hobbies);
+}

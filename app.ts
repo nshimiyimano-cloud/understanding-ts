d86eabfc9d44bea3263dbd164de0eba3a3c8fe53
nshimiyimana <njeanluc828@gmail.com>
2020-12-27 17:29:07 +0200

    never type to never return anything

--- app.ts
+++ app.ts
@@ -4,5 +4,16 @@ let userName:string;
 userInput=5;
 userInput='jeanluc';
 
-
+if(typeof userInput==='string'){
 userName=userInput;
+}
+
+//on never type
+//eg like this funct is by default even console again never return no thing(like undefined) so and as new feature in ts to make code very clear is to put never to say never return nthing if it throw it crashes  script
+
+function generateErrior(message:string,code:number):never{//never can be used on like this here and to show other dev that this funct never return value(more interesting here ever you hover you see is void) 
+    throw {message:message,errorCode:code}
+}
+
+ var result=generateErrior("an error Occured",500);
+ console.log(result);

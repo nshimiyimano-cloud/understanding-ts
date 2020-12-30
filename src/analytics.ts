64440a6d548f5e7d16b26ea68538258e0e1ba90f
nshimiyimana <njeanluc828@gmail.com>
2020-12-30 17:06:36 +0200

    we end strictNullCheck option

--- src/analytics.ts
+++ src/analytics.ts
@@ -1,3 +1,16 @@
 //this file is to hypothetically send analytic data for our server and we import it in index.html
 
 console.log("sending data..."); //we can send http requ to our analytic server
+
+//on strict where you can choose below aptions with your wishes or use only strict:true
+
+
+let logged;
+function sendAnalytics(data:string){ //here if param with no type if still be error  "noImplicitAny": true still be setted true if false no err to not define param type 
+    console.log(data);
+
+    logged=true;
+    
+    console.log(logged);
+}
+sendAnalytics("send analytic data");
--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,15 @@
 const names:string="nshimiyimana";
 
-const button=document.querySelector('button')!;
+const button=document.querySelector('button');
+
+//a good is to check if button has ot get a value this is good of "strictNullChecks":true  becouse check if the error found on button.addListener goes away
+
+
+if(button){ //it need developer to know if it not get null value
 button.addEventListener("click",()=>{
 console.log('button habe clicked');
 })
+}
 
 //const map=new Map();//if target js version is es5 this not recorganized by ts becouse this include in es6
 const personNames:string[]=["NSHIMIYIMANA","jeanluc","HABIHIRWE","johnus"];

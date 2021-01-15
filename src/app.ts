229eda3ccde3b234c54b543cde090ff2700fdd05
nshimiyimana <njeanluc828@gmail.com>
2021-01-15 21:40:54 +0200

    building more useful decoratowhre werender in html

--- src/app.ts
+++ src/app.ts
@@ -28,8 +28,31 @@ function Logger(logString:string){
 
 //when we excute our decorator we write it as function that has argument eg func() with ()
  /* if we not pre with@ constructor not be displayed on message be displayed other in decprator not */
-  @Logger("logging - Parson..")//if we write() we directly see in this () we pass in a string
+  //@Logger("logging - Parson..")//if we write() we directly see in this () we pass in a string
 
+
+
+
+  //  ----THEN WE ADD OR BUILD MORE USEFUL DECORATO
+
+  function WithTemplate(teampalte:string,hookId:string){  //here we wanty to render template which would be htmlcode into someplace in dom where i find this id
+      return function(constructor:any){
+const hookEl=document.getElementById(hookId);
+const p=new constructor();
+
+// check if var hookEl is a thing not undefined perfom some logic
+
+if(hookEl){
+    hookEl.innerHTML=teampalte;
+    document.querySelector('h2')!.textContent=p.name;
+}
+
+      }
+  }
+
+  //to excute our useful decorator
+
+  @WithTemplate('<h2>this is person object</h2>','app')//this 2nd arg is id from our div id in index.html
 
 
 

451af376c1088eec64d52af55f92eb78eb2a8ab1
nshimiyimana <njeanluc828@gmail.com>
2021-01-15 16:44:07 +0200

    decoratorfactorieswithsome arguments&anym function

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,38 @@
 
 //let start we can add class decorators is all about class
 
+/* 
 function Logger(constructor:Function){
 console.log('logging ....... ');
 console.log(constructor);    //this wil display our all that bwilds/construct(or class definition) our class parson in console
-}
 @Logger    //if you hover on this ts show you ''Logger' accepts too sfew arguments to be used as a decorator here. Did you mean to call it first and write '@Logger()'
+
+
+} */
+
+
+//then we change our function to decorator factory
+
+
+function Logger(logString:string){
+    return function(constructor:Function){
+        //to display message we will pass in our decorator when we excute it
+        console.log(logString);   //we pass this parameter instead of passe in a message
+
+        //and then display our construct as class definition
+        console.log(constructor);
+    }
+}
+
+
+//when we excute our decorator we write it as function that has argument eg func() with ()
+ /* if we not pre with@ constructor not be displayed on message be displayed other in decprator not */
+  @Logger("logging - Parson..")//if we write() we directly see in this () we pass in a string
+
+
+
+
+
 
 class Parson{
     

b0d11817c746dff1dfb8e69cc3d85f02a4ccae50
nshimiyimana <njeanluc828@gmail.com>
2021-01-15 11:43:00 +0200

    here no problem with union by parathesis()[]

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
-class dataStorage<T>{   //here we try to specify generic class for defining btype every where in this class  
-    private data:T[]=[];  //is lik to say string[] &here will store type(T) of array(T[])
-    addItem(item:T){   //here we get error becouse ts not know the type will be returned
+class dataStorage{   //here we try to specify generic class for defining btype every where in this class  
+    private data:(string |boolean |number)[]=[];  //we can use ()paratheses to group out all type as array type eg string[]
+    addItem(item:string|boolean | number){  //here because is primitive we don't need paratheses not array 
         this.data.push(item);
     }
-    removeItem(item:T){ //(indexOf(item),1) 1 means will remove only one item if not that 1 will remove all items
+    removeItem(item:string |boolean |number){ //(indexOf(item),1) 1 means will remove only one item if not that 1 will remove all items
 
         //to fix about if we pass object as item at call addItem() to make sure not remove wrong item when it loose its index instead of remove it return it without delete it
 
@@ -27,13 +27,13 @@ class dataStorage<T>{   //here we try to specify generic class for defining btyp
 
 //our class to make it more flexible we can give it possible to receive both type whether string or number,.. every type you want
 
-var textStoerage=new dataStorage<string | number | object>();
+var textStoerage=new dataStorage();    //here we remove generic way because we not use it above
 //textStoerage.addItem(20) //will thr error becouse is number
-/* textStoerage.addItem('max');//1st storage
+textStoerage.addItem('max');//1st storage
 textStoerage.addItem('emanuel') //2nd if we remove max item we will see only emanuel
-textStoerage.removeItem('max'); */
+textStoerage.removeItem('max');
 
-
+/* 
 //to pass in object
 textStoerage.addItem({name:'emanuel'})
 textStoerage.addItem({name:'max'});
@@ -41,7 +41,7 @@ textStoerage.addItem({name:'max'});
 //to remove object no primitive value as element of array will loose its index to delete it
 
 textStoerage.removeItem({name:'emanuel'}); //will reove wrong item
-
+ */
 console.log(textStoerage.getItem());
 
 

a43c79b331a2ad847ec1a7e927d699c774a38b12
nshimiyimana <njeanluc828@gmail.com>
2021-01-16 10:36:04 +0200

    we end to make dec work with title property

--- src/app.ts
+++ src/app.ts
@@ -16,6 +16,8 @@ console.log(constructor);    //this wil display our all that bwilds/construct(or
 
 
 function Logger(logString:string){
+
+    console.log('LOGGER FACTORY FUCTION'); //if we excute sth here in funct body without in its return template decora run last of logger decor sfactory fun
     return function(constructor:Function){
         //to display message we will pass in our decorator when we excute it
         console.log(logString);   //we pass this parameter instead of passe in a message
@@ -36,7 +38,14 @@ function Logger(logString:string){
   //  ----THEN WE ADD OR BUILD MORE USEFUL DECORATO
 
   function WithTemplate(teampalte:string,hookId:string){  //here we wanty to render template which would be htmlcode into someplace in dom where i find this id
-      return function(constructor:any){
+      
+
+    console.log('TEMPLATe FACTORY')
+    return function(constructor:any){
+
+
+        //then we see how decorator be excuted from last to first
+        console.log('lendering Template......');
 const hookEl=document.getElementById(hookId);
 const p=new constructor();
 
@@ -50,8 +59,12 @@ if(hookEl){
       }
   }
 
-  //to excute our useful decorator
+//we can add more than one decorato
 
+@Logger('Logging parson object....')
+
+  //to excute our useful decorator
+  
   @WithTemplate('<h2>this is person object</h2>','app')//this 2nd arg is id from our div id in index.html
 
 
@@ -70,3 +83,48 @@ const pers=new Parson;
 //if we visit page we see message  creating person object.....
 console.log(pers) //the we see message to create object parson  and output 
 //but above example no decoratos involved
+
+
+
+//otehr places we can add our decorator
+
+//thenn we add decorator decorator have argument depend on where will be used
+
+//here we'll need 2 args target as object will created/instantited and propertyNmae as property in our object
+function Log(target:any,propertyName:string | symbol){    //on target has any because we don't know which type our object will use/have,but property may be str or symbol
+
+console.log('property DECORATOR'); //we will seee when this runs
+console.log(target,propertyName);
+
+}
+
+
+//we add other class for our other decorat
+
+
+//to use our decorator
+
+
+class Product {
+@Log  //our log decorator used inside class
+    title:string;
+    private _price:number;
+
+    set price(val:number){
+        if(val>0){  //this check is for to not set negative price
+            this._price=val;
+        }
+        else{
+            throw  new Error('invalid price - should be positive');
+        }
+    }
+    constructor(t:string,p:number ) {
+        this.title=t;
+        this._price=p;
+    }
+
+    getPriceWithTax(tax:number){
+        return this._price*(1+tax);
+    }
+    
+}
